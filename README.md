9fc8e2503d0a44c0ee2bb28dec8efc847bf1dc2e
DiegoFourstage <diego.fourstage.github@gmail.com>
2020-05-01 14:39:35 -0300

    Conexão com o banco de dados

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # controle_de_vendas
 Controle de Vendas
+
+Commit 04
+Novo pacote model e sql, no pacote model nova classe Clientes e encapsulando atributos com seus geeter e setters, no pacote sql 
+adicionando novo banco de dados com nome de "projeto" e adicionando nova tabela de Clientes com seus elementos
+
+Commit 05
+Criando um novo pacote br.com.projeto.jdbc, a dentro método Connection getConnection em um try e catch String de conexão com o banco de dados
---
+++ src/br/com/projeto/jdbc/ConnectionFactory.java
@@ -0,0 +1,25 @@
+
+package br.com.projeto.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author D1350
+ */
+public class ConnectionFactory {
+    
+    public Connection getConnection(){
+        
+        // Tratamento de erros try e catch
+        try {
+            // Nossa String de conexão com o banco de dados
+            return DriverManager.getConnection("jdbc:mysql://localhost/projeto", "root", "");
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        
+    }
+    
+}
