4a2bf268ec92cdd28739e92f54d86089000e6542
DiegoFourstage <diego.fourstage.github@gmail.com>
2020-05-27 20:26:43 -0300

    Modelo Classe Produto(Chave Estrangeira Fornecedor)

---
+++ src/br/com/projeto/model/Produtos.java
@@ -0,0 +1,58 @@
+package br.com.projeto.model;
+
+/**
+ *
+ * @author D1350
+ */
+public class Produtos {
+
+    //Atributos
+    private int id;
+    private String descricao;
+    private double preco;
+    private int qtdEstoque;
+
+    //Getters e Setters    
+    private Fornecedor fornecedor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public void setQtdEstoque(int qtdEstoque) {
+        this.qtdEstoque = qtdEstoque;
+    }
+
+    public Fornecedor getFornecedor() {
+        return fornecedor;
+    }
+
+    public void setFornecedor(Fornecedor fornecedor) {
+        this.fornecedor = fornecedor;
+    }
+
+}
